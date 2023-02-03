235776fe97a7eb808c2a51edd800bdfb31256efa
pvl-mr <mariyapavlova2001@mail.ru>
2023-02-03 12:31:02 +0400

    v1 without db

---
+++ src/main/java/com/maripavlova/courseApp/controllers/TopicController.java
@@ -0,0 +1,42 @@
+package com.maripavlova.courseApp.controllers;
+
+import com.maripavlova.courseApp.resources.Topic;
+import com.maripavlova.courseApp.services.TopicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1")
+public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
+    @GetMapping("/topics")
+    public List<Topic> getAllTopics() {
+        return topicService.getTopics();
+    }
+
+    @GetMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @PostMapping("/topics")
+    public boolean addTopic(@RequestBody Topic topic) {
+        return topicService.addTopic(topic);
+    }
+
+    @PutMapping("/topics/{id}")
+    public boolean updateTopic(@PathVariable String id, @RequestBody Topic topic) {
+        return topicService.updateTopic(id, topic);
+    }
+
+    @DeleteMapping("/topics/{id}")
+    public boolean deleteTopic(@PathVariable String id) {
+        return topicService.deleteTopic(id);
+    }
+}
