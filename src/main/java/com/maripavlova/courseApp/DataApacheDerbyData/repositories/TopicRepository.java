2d8ce2d26ee2a7b616f04b033f5a4d0e8171b1c8
pvl-mr <mariyapavlova2001@mail.ru>
2023-02-03 13:02:25 +0400

    update

---
+++ src/main/java/com/maripavlova/courseApp/DataApacheDerbyData/repositories/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.maripavlova.courseApp.DataApacheDerbyData.repositories;
+
+import com.maripavlova.courseApp.DataApacheDerbyData.resources.Topic;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
