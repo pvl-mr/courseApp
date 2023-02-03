6695e728d78c905f341b1f5d0e172f6a5a54107c
pvl-mr <mariyapavlova2001@mail.ru>
2023-02-03 13:09:55 +0400

    hotfix

--- src/main/java/com/maripavlova/courseApp/DataApacheDerbyData/repositories/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package com.maripavlova.courseApp.DataApacheDerbyData.repositories;
-
-import com.maripavlova.courseApp.DataApacheDerbyData.resources.Topic;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {
-
-}
